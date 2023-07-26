6650df7547afa2492f0cb6f1080bcca010a0ee15
Kseniya Zemlyanko <shikaito@yandex.ru>
2023-07-26 15:33:26 +0300

    правка cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'https://shika.project.nomoredomains.xyz/' },
+    cors: { origin: 'https://shika.project.nomoredomains.xyz' },
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
